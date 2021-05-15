b0288f38f743759d11eb269b55269d2c0e260c32
pfpp89 <pfpp.89@gmail.com>
2021-05-15 10:00:58 +0200

    Adding the VS solution with the solution for both exercises
    Adding the VS solution with both exercises

---
+++ src/Exercises/Exercise1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static int countNumberOfPossibilities(int[] possibleValues, int maximumCount)
+        {
+            if (maximumCount < 1 || possibleValues == null || possibleValues.Length < 1)
+            {
+                // Protect against:
+                // > Requested maximum count being inferior to 0;
+                // > Null array of values supplied;
+                // > Empty array of values supplied.
+                return -1;
+            }
+
+            int[] countPerPossibleResult = new int[maximumCount + 1];
+
+            // base case
+            countPerPossibleResult[0] = 1;
+
+            // On the outer cycle, count until the desired value:
+            for (int i = 1; i <= maximumCount; i++)
+            {
+                // The inner cycle goes through the possible elements:
+                for (int j = 0; j < possibleValues.Length; j++)
+                {
+                    if (i >= possibleValues[j])
+                    {
+                        countPerPossibleResult[i] = countPerPossibleResult[i] + countPerPossibleResult[i - possibleValues[j]];
+                    }
+                }
+            }
+
+            // Return the number of ways to sum up to the value supplied:
+            return countPerPossibleResult[maximumCount];
+        }
+
+        // Call function for the case supplied:
+        public static void Main()
+        {
+            int sumValue = 26;
+            int[] possibilities = { 1, 2 };
+            
+
+            Console.Write("Result: " + countNumberOfPossibilities(possibilities, sumValue));
+        }
+    }
+}
---
+++ src/Exercises/Exercise2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = new int[] { 1, 1, 2, 3, 5, 6, 12, 13, 15, 15, 16, 18, 20, 27 };
+
+            int targetValue = 8;
+
+            bool canGetValue = checkForPairsThatSumUpTo(inputArray, targetValue);
+
+            Console.WriteLine("A sum of two elements that adds up to {0} {1} be obtained.",
+                targetValue,
+                canGetValue ? "can" : "cannot");
+        }
+
+        /// <summary>
+        /// This method will return true if the input array cntains at least
+        /// a pair of elements that adds up to the value supplied.
+        /// </summary>
+        /// <param name="inputArray">The array of integers where the check
+        /// will be performed.</param>
+        /// <param name="targetValue">The sum value we want to obtain.</param>
+        /// <returns>True if the sum of two array elements adds up to the
+        /// integer supplied, false otherwise.</returns>
+        private static bool checkForPairsThatSumUpTo(int[] inputArray, int targetValue)
+        {
+            // Check for validity:
+            if (inputArray==null)
+            {
+                return false;
+            }
+
+            // Create an hashset to store the differences:
+            HashSet<int> hs = new HashSet<int>();
+
+            // Cycle through elements of input array:
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                // Calculate the difference:
+                int temporaryValue = targetValue - inputArray[i];
+
+                if (hs.Contains(temporaryValue))
+                {
+                    // A sum has been found, no need to continue:
+                    return true;
+                }
+
+                hs.Add(inputArray[i]);
+            }
+
+            // Arriving here, no valid sum has been found:
+            return false;
+        }
+    }
+}
